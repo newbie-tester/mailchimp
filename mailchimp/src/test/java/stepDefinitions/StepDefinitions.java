623174eeb5ed9444e607367f61c2787c5e35fa6a
Eva <evaling.hk@gmail.com>
2021-04-06 13:44:15 +0200

    Add all test cases
    All 4 scenarios in feature file, with absolute test data in
    StepDefinitions.

--- mailchimp/src/test/java/stepDefinitions/StepDefinitions.java
+++ mailchimp/src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,9 +15,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
-	
+
 	private WebDriver driver;
-	
+
 	@Given("I navigate to the sign-up page")
 	public void i_navigate_to_the_sign_up_page() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
@@ -29,16 +30,17 @@ public class StepDefinitions {
 //		cookieAccept.click();
 	}
 
-	@Given("enter valid email address, username and password")
-	public void enter_valid_email_address_username_and_password() {
-	   WebElement email = driver.findElement(By.id("email"));
-	   email.sendKeys("hej@hej.com");
-	   WebElement username = driver.findElement(By.id("new_username"));
-	   username.sendKeys("hej@hej.com");
-	   WebElement password = driver.findElement(By.id("new_password"));
-	   password.sendKeys("Abcdef+1");
+	@Given("enter email address, username and password")
+	public void enter_email_address_username_and_password() {
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("hej@hej.com");
+		WebElement username = driver.findElement(By.id("new_username"));
+		username.sendKeys(
+				"Averylongusernameover100charactersAverylongusernameover100charactersAverylongusernameover100characters");
+		WebElement password = driver.findElement(By.id("new_password"));
+		password.sendKeys("Abcdef+1");
 	}
-	
+
 	@When("I click on the sign-up button")
 	public void i_click_on_the_sign_up_button() {
 		click(driver, By.id("create-account"));
@@ -47,20 +49,29 @@ public class StepDefinitions {
 //				presenceOfElementLocated(By.id("create-account")));
 //		signUpButton.click();
 	}
-	
+
 	@Then("I am registered as a user")
 	public void i_am_registered_as_a_user() {
-	    WebElement successPage = (new WebDriverWait(driver, 10)).until(ExpectedConditions.
-				presenceOfElementLocated(By.tagName("h1")));
-	    assertEquals("Check your email", successPage.getAttribute("textContent"));
-	    driver.quit();
+		WebElement successPage = (new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.tagName("h1")));
+		assertEquals("Check your email", successPage.getAttribute("textContent"));
+		driver.quit();
+	}
+
+	@Then("I get an error message")
+	public void i_get_an_error_message() {
+		WebElement errorMessage = driver.findElement(By.className("invalid-error"));
+//		assertTrue(errorMessage.getAttribute("textContent").contains("username already exists"));
+//		assertEquals("Please enter a value", errorMessage.getAttribute("textContent"));
+		assertEquals("Enter a value less than 100 characters long", errorMessage.getAttribute("textContent"));
+		driver.quit();
 	}
 
 	private void click(WebDriver driver, By by) {
 
-		(new WebDriverWait(driver,10)).until(ExpectedConditions.elementToBeClickable(by));
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 		driver.findElement(by).click();
 
-		}
-	
+	}
+
 }
--- mailchimp/src/test/resources/Features/signup.feature
+++ mailchimp/src/test/resources/Features/signup.feature
@@ -2,12 +2,29 @@ Feature: Sign-up function on mailchimp.com
   The user should be able to sign-up when valid credentials are provided.
   If any of the credentials do not fulfill the requirements, error messages should appear indicating what is wrong.
 
-  Scenario: Sign-up with valid credentials
-    Given I navigate to the sign-up page
-    And enter valid email address, username and password
-    When I click on the sign-up button
-    Then I am registered as a user
+Scenario: Sign-up with valid credentials
+	Given I navigate to the sign-up page
+	And enter email address, username and password
+	When I click on the sign-up button
+	Then I am registered as a user
   
+Scenario: Sign-up with existing username
+	Given I navigate to the sign-up page
+	And enter email address, username and password
+	When I click on the sign-up button
+	Then I get an error message
+
+Scenario: Sign-up without email
+	Given I navigate to the sign-up page
+	And enter email address, username and password
+	When I click on the sign-up button
+	Then I get an error message
+  
+Scenario: Sign-up with long username
+	Given I navigate to the sign-up page
+	And enter email address, username and password
+	When I click on the sign-up button
+	Then I get an error message
 
  #   Examples: 
  #     | name  | value | status  |
